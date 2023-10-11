1c3fa345b98968d784060a020f74299f7a915a74
송 유진 <tlahsh12@naver.com>
2023-10-11 23:11:55 +0900

    Order Repo findAll 로직 추가, Order 검색 조건

---
+++ src/main/java/jpabook/jpashop/domain/OrderSearch.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain;
+
+public class OrderSearch {
+
+    private String membername;
+    private OrderStatus orderStatus;
+
+    public String getMembername() {
+        return membername;
+    }
+
+    public void setMembername(String membername) {
+        this.membername = membername;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+}
